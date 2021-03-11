fb2ce92705509db0ae1f57f23f949fde5838fd17
aulichney <annie.ulichney@yale.edu>
2021-03-11 12:34:51 -0500

    Fixed time served

--- bundle.js
+++ bundle.js
@@ -25,7 +25,8 @@
     	.rollup(d => {
         return {
           amount: d.length,
-  				ageAvg: d3.mean(d.map(correspondent => correspondent.age))
+  				ageAvg: d3.mean(d.map(correspondent => correspondent.age)),
+          avgTimeServed: d3.mean(d.map(correspondent => correspondent.timeServed))
         }
       })
   		.entries(source);
@@ -43,7 +44,8 @@
     return {
       sex: row.sex,
       age: Number(row.age),
-      raceEthnicity: row.raceEthnicity
+      raceEthnicity: row.raceEthnicity,
+      timeServed: row.timeServed
 
     }
   }
@@ -54,7 +56,7 @@
   }
 
   //Both json and csv gist urls -- set up for json currently
-  const dataUrl = 'https://gist.githubusercontent.com/aulichney/d4589c85658f1a2248b143dfd62005b4/raw/5c95164db1fa7369dd12d67b7e7bf62244e576ba/undercustodymod.json';
+  const dataUrl = 'https://gist.githubusercontent.com/aulichney/d4589c85658f1a2248b143dfd62005b4/raw/767be748b7cb3e067aacfc625c045847a73c003f/undercustodymod.json';
   //const dataUrl = 'https://gist.githubusercontent.com/aulichney/f60a198f6551aaafd29a91c56f70a184/raw/7e3cfb21baa0e3b6d6b3f86cae58b36bdb46ecdc/undercustodymod.csv'
 
   const svg = d3$1.select('svg');
